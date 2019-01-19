2a37103f2acad6e76a99bb76926b08ddc67a824f
plumbblake <plumbblake@users.noreply.github.com>
2019-01-19 09:23:51 -0700

    Added resume to site

--- components/bp-headings.js
+++ components/bp-headings.js
@@ -1,4 +1,5 @@
 import spreadAttributes from "../js/spreadAttributes.js";
+import "../components/bp-stack.js";
 
 class H1 extends HTMLElement {
   connectedCallback() {
@@ -8,6 +9,7 @@ class H1 extends HTMLElement {
         h1 {
             margin: 0;
             font-size: 32px;
+            font-weight: 700;
             line-height: 1.2;
             font-family: "Space Mono", sans-serif;
         } 
@@ -40,7 +42,7 @@ class H2 extends HTMLElement {
           h2 {
               margin: 0;
               font-size: 28px;
-              font-weight: 300;
+              font-weight: 400;
               line-height: 1.2;
               font-family: "Space Mono", sans-serif;
           } 
@@ -64,3 +66,60 @@ class H2 extends HTMLElement {
 }
 
 customElements.define("bp-heading2", H2);
+
+class H3 extends HTMLElement {
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: "open" });
+
+    const styles = `
+            h3 {
+                margin: 0;
+                font-size: 20px;
+                font-weight: 400;
+                line-height: 1.2;
+                font-family: "Space Mono", sans-serif;
+            } 
+
+            hr {
+                display: inline-block;
+                width: 100%;
+                max-width: 200px;
+                border-bottom: 1px solid;
+                margin: 0;
+            }
+        `;
+
+    shadowRoot.innerHTML = `
+                <style>${styles}</style>
+                <bp-stack gap-size="4px">
+                    <h3 ${spreadAttributes(this.attributes)}><slot></slot></h3>
+                    <hr />
+                </bp-stack>
+            `;
+  }
+}
+
+customElements.define("bp-heading3", H3);
+
+class H4 extends HTMLElement {
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: "open" });
+
+    const styles = `
+              h4 {
+                  margin: 0;
+                  font-size: 16px;
+                  font-weight: 700;
+                  line-height: 1.2;
+                  font-family: "Open Sans", sans-serif;
+              } 
+          `;
+
+    shadowRoot.innerHTML = `
+                  <style>${styles}</style>
+                  <h4 ${spreadAttributes(this.attributes)}><slot></slot></h4>
+              `;
+  }
+}
+
+customElements.define("bp-heading4", H4);
